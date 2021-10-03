a06bca573a9e162b38bef13c155a8ffbee23c5fa
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-10-02 22:35:43 -0500

    added

--- models/fetch-model.js
+++ models/fetch-model.js
@@ -1,6 +1,22 @@
 var mongoose=require('mongoose');
 var db = require('../database');
 // create an schema
-
+var userSchema = new mongoose.Schema({
+            username: String,
+            Password:String,
+            Message:String,
+            
+        });
 userTable=mongoose.model('users',userSchema);
         
+module.exports={
+     
+     fetchData:function(callback){
+        var userData=userTable.find({});
+        userData.exec(function(err, data){
+            if(err) throw err;
+            return callback(data);
+        })
+        
+     }
+}
