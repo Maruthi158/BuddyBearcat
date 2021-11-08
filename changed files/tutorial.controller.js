61b9610c87af57e3165a7e6a2fe8d84976eb112e
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-11-08 10:27:05 -0600

    Updating the controller create the instance in database

--- changed files/tutorial.controller.js
+++ changed files/tutorial.controller.js
@@ -1,1 +1,19 @@
-
+const db = require("../models");
+const Tutorial = db.tutorials;
+  // Create and Save a new Tutorial
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.username) {
+      res.status(400).send({ message: "Content can not be empty!" });
+      return;
+    }
+  
+    // Create a Tutorial
+    const tutorial = new Tutorial({
+      username: req.body.username,
+      pass: req.body.pass,
+      Reenterpassword: req.body.Reenterpassword,
+      entermobileno: req.body.entermobileno,
+      enteremailid: req.body.enteremailid,
+      isloggedin: req.body.isloggedin ? req.body.isloggedin : false
+    });
