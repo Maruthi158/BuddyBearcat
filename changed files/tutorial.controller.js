1218f09c3ecadccf462189a4740927a3f29f9705
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-11-08 10:28:22 -0600

    finding and Deleting a particular id

--- changed files/tutorial.controller.js
+++ changed files/tutorial.controller.js
@@ -47,3 +47,46 @@ exports.findAll = (req, res) => {
         });
       });
   };
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+  
+    Tutorial.findById(id)
+      .then(data => {
+        if (!data)
+          res.status(404).send({ message: "Not found user with id " + id });
+        else res.send(data);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .send({ message: "Error retrieving user with id=" + id });
+      });
+  };
+
+// Update a Tutorial by the id in the request
+exports.update = (req, res) => {
+    if (!req.body) {
+      return res.status(400).send({
+        message: "Data to update can not be empty!"
+      });
+    }
+  
+    const id = req.params.id;
+  
+    Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+      .then(data => {
+        if (!data) {
+          res.status(404).send({
+            message: `Cannot update user with id=${id}. Maybe user was not found!`
+          });
+        } else res.send({ message: "user was updated successfully." });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating user with id=" + id
+        });
+      });
+  };
+
