6af652cb92186118c430fea02a381bee96846610
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-11-08 10:27:48 -0600

    Updating logic to retrieve all the tutorials

--- changed files/tutorial.controller.js
+++ changed files/tutorial.controller.js
@@ -17,3 +17,33 @@ exports.create = (req, res) => {
       enteremailid: req.body.enteremailid,
       isloggedin: req.body.isloggedin ? req.body.isloggedin : false
     });
+ // Save Tutorial in the database
+    tutorial
+      .save(tutorial)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the user."
+        });
+      });
+  };
+
+// Retrieve all Tutorials from the database.
+exports.findAll = (req, res) => {
+    const title = req.query.username;
+    var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+  
+    Tutorial.find(condition)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving user."
+        });
+      });
+  };
