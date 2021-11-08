9a0488d3385171b5586676899828a9123eece14e
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-11-08 10:33:46 -0600

    Creating table details in model.js

--- changed files/tutorial.model.js
+++ changed files/tutorial.model.js
@@ -1,1 +1,19 @@
 
+module.exports = mongoose => {
+const Tutorial = mongoose.model(
+    "tutorial",
+    mongoose.Schema(
+      {
+        username: String,
+        pass: String,
+        Reenterpassword: String,
+        entermobileno:String,
+        enteremailid:String,
+        isloggedin: Boolean
+      },
+      { timestamps: true }
+    )
+  );
+
+  return Tutorial;
+};
